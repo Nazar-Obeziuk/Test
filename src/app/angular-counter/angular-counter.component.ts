9fe39e8200a6079329435b245cc5d297729058a9
Nazar Obeziuk <nazarobeziuk@gmail.com>
2023-10-27 18:57:00 +0200

    changes

--- src/app/angular-counter/angular-counter.component.ts
+++ src/app/angular-counter/angular-counter.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AngularCounterComponent {
 
+  public counter: number;
+
+  constructor() {
+    this.counter = 0;
+  }
+
+  countUp(number: number): void {
+    this.counter += number;
+  }
+
 }
