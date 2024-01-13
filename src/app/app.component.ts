f021be545811e9110917fb59cffdd392520eca7b
Nazar Obeziuk <nazarobeziuk@gmail.com>
2024-01-13 18:29:43 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+declare var google: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+      // client_id: '298527698559-csqs17fneirjhta3d9lgsghgd1rv99i2.apps.googleusercontent.com',
+      client_id: '298527698559-dr2gnkq2lhsj5ftfmpnr5o30kvbekfqh.apps.googleusercontent.com',
+      callback: (resp: any) => {
+        console.log(resp);
+        
+      }
+    });
+
+    google.accounts.id.renderButton(document.getElementById('google-btn'), {
+      theme: 'filled_blue',
+      size: 'large',
+      shape: 'rectangle',
+      width: 350
+    })
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
